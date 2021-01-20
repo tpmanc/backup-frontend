7a7e012ef5f601f01869bfb90c27128c4f0f7feb
tpmanc <tpxtrime@mail.ru>
2021-01-20 11:31:51 +0300

    try

--- src/components/echo.js
+++ src/components/echo.js
@@ -2,26 +2,27 @@ import Echo from 'laravel-echo'
 import { io } from 'socket.io-client'
 import { jwtHelper } from '../helpers/jwtHelper'
 
-// window.Pusher = require('pusher-js');
-const socket = io
-window.io = socket
+// const socket = io
+// window.io = socket
+window.io = require('socket.io-client');
 
 // const AppEcho = new Echo({
 //     broadcaster: 'socket.io',
 //     host: 'http://api.backup.wsdxz.ru' + ':6001' // this is laravel-echo-server host
 // })
-window.Test = Echo
+// window.Test = Echo
 
 let echo = new Echo({
     broadcaster: 'socket.io',
-    host: 'api.backup.wsdxz.ru:6001', // значение должно быть равным authHost из конфига + порт
+    host: 'backup.wsdxz.ru:6001', // значение должно быть равным authHost из конфига + порт
+    // transports: ['websocket', 'polling', 'flashsocket'],
     auth: {
         headers: {
-            Authorization: "Bearer " + jwtHelper.getToken()
+            'Authorization': "Bearer " + jwtHelper.getToken()
         }
     }
 });
-console.log(echo)
+// console.log(echo)
 
 echo
     .private('test')
